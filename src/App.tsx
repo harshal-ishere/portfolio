8031a5e66d68e519f4d130e4eb1515f5644ac363
unknown <harshal011kamble@gmail.com>
2023-05-28 16:08:06 +0530

    made a decent background layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import Navbar from "./components/navbar/index";
+import Body from "./components/body/index";
 function App() {
-  return (
-    <div className="App">
-      <h1>hello my name is harshal</h1>
-    </div>
+  return ( 
+      <div className="App">
+        <div className="row">
+          <div className="col-2 pe-0">
+          <Navbar />
+          </div>
+          <div className="col-10 ps-0">
+           <Body />
+          </div>
+        </div>
+      </div>
+
   );
 }
 
---
+++ src/components/navbar/index.tsx
@@ -0,0 +1,10 @@
+import {FC} from "react";
+import "./styles.css"
+let Navbar:FC=()=>{
+    return(
+        <div className="navu">
+           <p>harshal.ishere</p>
+        </div>
+    )
+}
+export default Navbar;
